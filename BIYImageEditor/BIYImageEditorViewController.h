3495ed5ce97871675028d536fbd4e60e96b25636
Heejae Kim <lp90122@linecorp.com>
2016-04-29 17:37:08 +0900

    set default storyboard and add ImageEditorViewController

---
+++ BIYImageEditor/BIYImageEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIYImageEditorViewController.h
+//  BIYImageEditor
+//
+//  Created by mulberry on 2016. 4. 29..
+//  Copyright © 2016년 bisolby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIYImageEditorViewController : UIViewController
+
+@end
