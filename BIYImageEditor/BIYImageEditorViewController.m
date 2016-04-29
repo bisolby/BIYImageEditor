35969c6183dded69e8569771216410826deef93b
Heejae Kim <lp90122@linecorp.com>
2016-04-29 18:46:20 +0900

    porting original swift code to objective-c code

--- BIYImageEditor/BIYImageEditorViewController.m
+++ BIYImageEditor/BIYImageEditorViewController.m
@@ -8,30 +8,224 @@
 
 #import "BIYImageEditorViewController.h"
 
-@interface BIYImageEditorViewController ()
+static CGFloat maximumScale = 10.f;
+
+@interface BIYImageEditorViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *imageView;
+@property (weak, nonatomic) IBOutlet UIView *areaView;
+
+@property (strong, nonatomic) UIImage *originalImage;
+@property (strong, nonatomic) UIImageView *originalImageView;
 
 @end
 
 @implementation BIYImageEditorViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    _originalImage = [UIImage imageNamed:@"2.jpg"];
+    _originalImageView = [UIImageView new];
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    _scrollView.minimumZoomScale = 1;
+    _scrollView.maximumZoomScale = 100;
+    
+    _imageView.userInteractionEnabled = NO;
+    
+    _areaView.layer.borderColor = [[UIColor whiteColor] colorWithAlphaComponent:.8f].CGColor;
+    _areaView.layer.borderWidth = 2.f / [UIScreen mainScreen].scale;
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    CGFloat scaleToFit = 1.f;
+    
+    if (_originalImage.size.width > _originalImage.size.height)
+    {
+        scaleToFit = CGRectGetHeight(_areaView.frame) / _originalImage.size.height;
+        
+        CGRect originalImageViewFrame = _originalImageView.frame;
+        originalImageViewFrame.size.width = _originalImage.size.width * scaleToFit;
+        originalImageViewFrame.size.height = CGRectGetHeight(_areaView.frame);
+        _originalImageView.frame = originalImageViewFrame;
+    }
+    else
+    {
+        scaleToFit = CGRectGetWidth(_areaView.frame) / _originalImage.size.width;
+        
+        CGRect originalImageViewFrame = _originalImageView.frame;
+        originalImageViewFrame.size.width = CGRectGetHeight(_areaView.frame);
+        originalImageViewFrame.size.height = _originalImage.size.height * scaleToFit;
+        _originalImageView.frame = originalImageViewFrame;
+    }
+    
+    _originalImageView.image = _originalImage;
+    _originalImageView.center = _scrollView.center;
+    _originalImageView.userInteractionEnabled = NO;
+    
+    [_scrollView addSubview:_originalImageView];
+    
+    _scrollView.contentSize = _originalImageView.bounds.size;
+    
+    CGFloat overWidth = _scrollView.contentSize.width - _areaView.frame.size.width;
+    if (overWidth > 0)
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.frame.size.width + overWidth, _scrollView.contentSize.height)];
+    }
+    else
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.frame.size.width, _scrollView.contentSize.height)];
+    }
+    
+    CGFloat overHeight = _scrollView.contentSize.height - _areaView.frame.size.height;
+    if (overHeight > 0)
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.contentSize.width, _scrollView.frame.size.height + overHeight)];
+    }
+    else
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.contentSize.width, _scrollView.frame.size.height)];
+    }
+    
+    CGRect originalImageViewFrame = _originalImageView.frame;
+    originalImageViewFrame.origin.x = _scrollView.contentSize.width / 2.f - _originalImageView.frame.size.width / 2.f;
+    originalImageViewFrame.origin.y = _scrollView.contentSize.height / 2.f - _originalImageView.frame.size.height / 2.f;
+    _originalImageView.frame = originalImageViewFrame;
+    
+    if (_originalImage.size.width > _originalImage.size.height)
+    {
+        [_scrollView setContentOffset:CGPointMake(CGRectGetWidth(_originalImageView.frame) / 2 - CGRectGetWidth(_areaView.frame) / 2, _scrollView.contentOffset.y)];
+    }
+    else
+    {
+        [_scrollView setContentOffset:CGPointMake(_scrollView.contentOffset.x, CGRectGetHeight(_originalImageView.frame) / 2 - CGRectGetHeight(_areaView.frame) / 2)];
+    }
+
+    _imageView.layer.mask = [[UIImageView alloc] initWithImage:[self createMaskImage]].layer;
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - User Default Mathods
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+- (UIImage *)createMaskImage
+{
+    UIView *rectView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    rectView.backgroundColor = [UIColor blackColor];
+    
+    UIView *circleView = [[UIView alloc] initWithFrame:CGRectMake(CGRectGetMinX(_areaView.frame) + 1.0,
+                                                                  CGRectGetMinY(_areaView.frame) + 1.0,
+                                                                  CGRectGetWidth(_areaView.frame) - 1.0,
+                                                                  CGRectGetHeight(_areaView.frame) - 1.0)];
+    circleView.layer.masksToBounds = YES;
+    circleView.layer.cornerRadius = CGRectGetWidth(_areaView.frame) / 2.0;
+    circleView.center = rectView.center;
+    circleView.backgroundColor = [UIColor whiteColor];
+    
+    [rectView addSubview:circleView];
+    
+    UIGraphicsBeginImageContextWithOptions(rectView.bounds.size, false, 0.0);
+    [rectView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *maskImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    CGImageRef masked = CGImageCreateWithMask(maskImage.CGImage, CGImageMaskCreate(CGImageGetWidth(maskImage.CGImage),
+                                                                                   CGImageGetHeight(maskImage.CGImage),
+                                                                                   CGImageGetBitsPerComponent(maskImage.CGImage),
+                                                                                   CGImageGetBitsPerPixel(maskImage.CGImage),
+                                                                                   CGImageGetBytesPerRow(maskImage.CGImage),
+                                                                                   CGImageGetDataProvider(maskImage.CGImage), nil, false));
+    
+    UIImage *image = [[UIImage alloc] initWithCGImage:masked scale:[UIScreen mainScreen].scale orientation:UIImageOrientationUp];
+    
+    return image;
 }
-*/
+
+- (UIImage *)clipImage
+{
+    UIGraphicsBeginImageContextWithOptions([UIScreen mainScreen].bounds.size, false, 0.0);
+    
+    self.imageView.hidden = YES;
+    
+    [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    
+    self.imageView.hidden = NO;
+    
+    UIImage *baseImage = UIGraphicsGetImageFromCurrentImageContext();
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CGRect rect = CGRectMake(CGRectGetMinX(_areaView.frame) * scale,
+                             CGRectGetMinY(_areaView.frame) * scale,
+                             CGRectGetWidth(_areaView.frame) * scale,
+                             CGRectGetHeight(_areaView.frame) * scale);
+    CGImageRef clipImage = CGImageCreateWithImageInRect(baseImage.CGImage, rect);
+    
+    UIGraphicsEndImageContext();
+    
+    return [[UIImage alloc] initWithCGImage:clipImage];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return _originalImageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    CGFloat overWidth = _scrollView.contentSize.width - _areaView.frame.size.width;
+    if (overWidth > 0)
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.frame.size.width + overWidth, _scrollView.contentSize.height)];
+    }
+    else
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.frame.size.width, _scrollView.contentSize.height)];
+    }
+    
+    CGFloat overHeight = _scrollView.contentSize.height - _areaView.frame.size.height;
+    if (overHeight > 0)
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.contentSize.width, _scrollView.frame.size.height + overHeight)];
+    }
+    else
+    {
+        [_scrollView setContentSize:CGSizeMake(_scrollView.contentSize.width, _scrollView.frame.size.height)];
+    }
+    
+    CGRect originalImageViewFrame = _originalImageView.frame;
+    originalImageViewFrame.origin.x = _scrollView.contentSize.width / 2.f - _originalImageView.frame.size.width / 2.f;
+    originalImageViewFrame.origin.y = _scrollView.contentSize.height / 2.f - _originalImageView.frame.size.height / 2.f;
+    _originalImageView.frame = originalImageViewFrame;
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale
+{
+    if (scrollView.zoomScale > maximumScale)
+    {
+        [scrollView setZoomScale:maximumScale animated:YES];
+    }
+}
 
 @end
