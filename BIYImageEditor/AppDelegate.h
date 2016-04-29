82c0f7fd52c6186a872fb56240a855d15ca525e5
Heejae Kim <lp90122@linecorp.com>
2016-04-29 17:28:58 +0900

    add init project

---
+++ BIYImageEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BIYImageEditor
+//
+//  Created by mulberry on 2016. 4. 29..
+//  Copyright © 2016년 bisolby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
